421f0c9fec891c14dcaee7444ebc579418edad17
DJ5harma <sunubhargav2004@gmail.com>
2025-04-23 17:04:17 +0530

    added 2 team members and changed some code

--- components/sections/Team.php
+++ components/sections/Team.php
@@ -1,129 +1,145 @@
-<div style="display: flex; flex-direction: column; width: 100%; height:100%; align-items: center;">
-    <h1 class="heading">Our Team</h1>
-    <div id="Team">
-        <div>
-            <img src="images/drnikhil.jpg" alt="Dr. Nikhil Marriwala">
-            <div>
-                <b>Dr. Nikhil Marriwala</b>
-                <p>Training & Placement Officer</p>
-                <p>Contact: 9416668482</p>
-                <p>Email ID: nmarriwala@kuk.ac.in</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/drsanjeev.jpg" alt="Dr. Sanjeev Ahuja">
-            <div>
-                <b>Dr. Sanjeev Ahuja</b>
-                <p>Training & Placement Officer</p>
-                <p>Contact: 9991385634</p>
-                <p>Email ID: sahuja2015@kuk.ac.in</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/DrSunilDhingra.jpeg" alt="Dr. Sunil Dhingra">
-            <div>
-                <b>Dr. Sunil Dhingra</b>
-                <p>Training & Placement Officer</p>
-                <p>Contact: 9541653750</p>
-                <p>Email ID: sdhingra2020@gmail.com</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/shikha_cse.jpg" alt="Shikha Chaudhry">
-            <div>
-                <b>Shikha Chaudhry</b>
-                <p>Assistant Professor</p>
-                <p>Contact: 01744239155</p>
-                <p>Email ID: shikhachaudhry107@gmail.com</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/Pragya.jpg" alt="Pragya Chandi">
-            <div>
-                <b>Pragya Chandi</b>
-                <p>Assistant Professor</p>
-                <p>Contact: 01744239155</p>
-                <p>Email ID: pragya.cse.uiet@kuk.ac.in</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/krishanpandey.jpg" alt="MR. Krishna Pandey">
-            <div>
-                <b>MR. Krishna Pandey</b>
-                <p>Assistant Professor</p>
-                <p>Contact: 9416077614</p>
-                <p>Email ID: kp7636@gmail.com</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/krishangopal.jpg" alt="MR. Krishan Gopal">
-            <div>
-                <b>MR. Krishan Gopal</b>
-                <p>Assistant Professor</p>
-                <p>Contact: 9034353879</p>
-                <p>Email ID: kgopal2015@kuk.ac.in</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/Vikas.jpg" alt="Vikas">
-            <div>
-                <b>MR. Vikas</b>
-                <p>Assistant Professor</p>
-                <p>Contact: 9729012010</p>
-                <p>Email ID: nirmaldhotia@gmail.com</p>
-            </div>
-        </div>
-        <div>
-            <img src="images/Archit-Sharma.jpg" alt="Archit Sharma">
-            <div>
-                <b>MR. Archit Sharma</b>
-                <p>Assistant Professor</p>
-                <p>Contact: 9466814700</p>
-                <p>Email ID: archit.sharma786@gmail.com</p>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Dr. Sanjeev Ahuja (TPO) , 
-Mr. Archi -->
+<?php
+$team = [
+    [
+        "name" => "Dr. Nikhil Marriwala",
+        "role" => "Training & Placement Officer",
+        "contact" => "9416668482",
+        "email" => "nmarriwala@kuk.ac.in",
+        "image" => "images/drnikhil.jpg"
+    ],
+    [
+        "name" => "Dr. Sanjeev Ahuja",
+        "role" => "Training & Placement Officer",
+        "contact" => "9991385634",
+        "email" => "sahuja2015@kuk.ac.in",
+        "image" => "images/drsanjeev.jpg"
+    ],
+    [
+        "name" => "Dr. Sunil Dhingra",
+        "role" => "Training & Placement Officer",
+        "contact" => "9541653750",
+        "email" => "sdhingra2020@gmail.com",
+        "image" => "images/DrSunilDhingra.jpeg"
+    ],
+    [
+        "name" => "Mr. Ajay Jangra",
+        "role" => "Training & Placement Officer",
+        "contact" => "7082113149",
+        "email" => "er_jangra@yahoo.co.in",
+        "image" => "images/ajay_jangra.jpg"
+    ],
+    [
+        "name" => "Dr. Deepak Malik",
+        "role" => "Training & Placement Officer",
+        "contact" => "9017414672",
+        "email" => "deepak111mallick@gmail.com",
+        "image" => "images/deepakmalik.jpg"
+    ],
+    [
+        "name" => "Shikha Chaudhry",
+        "role" => "Assistant Professor",
+        "contact" => "01744239155",
+        "email" => "shikhachaudhry107@gmail.com",
+        "image" => "images/shikha_cse.jpg"
+    ],
+    [
+        "name" => "Pragya Chandi",
+        "role" => "Assistant Professor",
+        "contact" => "01744239155",
+        "email" => "pragya.cse.uiet@kuk.ac.in",
+        "image" => "images/Pragya.jpg"
+    ],
+    [
+        "name" => "Mr. Krishna Pandey",
+        "role" => "Assistant Professor",
+        "contact" => "9416077614",
+        "email" => "kp7636@gmail.com",
+        "image" => "images/krishanpandey.jpg"
+    ],
+    [
+        "name" => "Mr. Krishan Gopal",
+        "role" => "Assistant Professor",
+        "contact" => "9034353879",
+        "email" => "kgopal2015@kuk.ac.in",
+        "image" => "images/krishangopal.jpg"
+    ],
+    [
+        "name" => "Mr. Vikas",
+        "role" => "Assistant Professor",
+        "contact" => "9729012010",
+        "email" => "nirmaldhotia@gmail.com",
+        "image" => "images/Vikas.jpg"
+    ],
+    [
+        "name" => "Mr. Archit Sharma",
+        "role" => "Assistant Professor",
+        "contact" => "9466814700",
+        "email" => "archit.sharma786@gmail.com",
+        "image" => "images/Archit-Sharma.jpg"
+    ]
+];
+
+?>
+
+
 <style>
     #Team {
         display: flex;
         width: 100%;
         flex-direction: column;
         height: 100%;
-        /* flex-wrap: wrap; */
         overflow: auto;
-
-        align-items: center;
         justify-content: start;
-
         gap: 20px;
-        padding: 10px 0;
+        padding: 10px;
+    }
 
-        div {
-            display: flex;
-            background-color: rgb(31, 65, 185);
-            border-radius: 20px;
+    #Team>div {
+        display: flex;
+        background-color: rgb(31, 65, 185);
+        border-radius: 20px;
+    }
 
-            img {
-                border-radius: 20px;
-                width: 300px;
-            }
+    #Team img {
+        border-radius: 20px;
+        width: 300px;
+    }
 
-            div {
-                display: flex;
-                flex-direction: column;
-                gap: 10px;
-                justify-content: center;
-                padding: 0 30px;
+    #Team>div>div {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        justify-content: center;
+        padding: 0 30px;
+    }
 
-                p,
-                b {
-                    font-size: larger;
-                    color: aliceblue;
-                }
-            }
-        }
+    #Team p,
+    #Team b {
+        font-size: larger;
+        color: aliceblue;
+    }
+
+    .heading {
+        text-align: center;
+        font-size: 2rem;
     }
 </style>
+
+<div style="display: flex; flex-direction: column; width: 100%; height:100%;">
+    <h1 class="heading">Our Team</h1>
+    <div id="Team">
+        <?php foreach ($team as $member): ?>
+            <div>
+                <img src="<?= $member['image'] ?>" alt="<?= $member['name'] ?>">
+                <div>
+                    <b><?= $member['name'] ?></b>
+                    <p><?= $member['role'] ?></p>
+                    <p>Contact: <?= $member['contact'] ?></p>
+                    <p>Email ID: <?= $member['email'] ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+</html>
