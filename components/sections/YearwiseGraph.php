f5fcaa3e057915a9faf697e9b3d156cb9e940041
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2025-04-29 23:21:00 +0530

    replacing with static image to prevent loading wait time

--- components/sections/YearwiseGraph.php
+++ components/sections/YearwiseGraph.php
@@ -1,15 +1,15 @@
 <div id="YearwiseGraph">
     <h1 class="heading">Our Yearwise Graph</h1>
     <h2>Average Package by year (in LPA)</h2>
-    <iframe
+    <!-- <iframe
         src="https://chart-generator.draxlr.com/embed/uxXvhBaOxVv6qfjT6l2x3EQeDthLcgQg"
         loading="lazy"
         title="Avg package bar chart"
         frameborder="0"
-    ></iframe>
+    ></iframe> -->
 
-    <!-- Incase the iframe fails in the future, uncomment the line below to load the static png of the interactive graph and comment the iframe -->
-    <!-- <img src="data/YearwiseAveragePackageGraph.png" alt="Avg. package year-wise graph"> -->
+    <!-- Incase the iframe fails in the future, uncomment the line below to load the static png of the interactive graph and comment the iframe (or vice versa) -->
+    <img src="data/YearwiseAveragePackageGraph.png" alt="Avg. package year-wise graph">
 </div>
 <style>
     #YearwiseGraph {
